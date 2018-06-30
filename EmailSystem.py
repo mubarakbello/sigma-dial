23e4a603573e536a427db92db26ee95b32871e86
Bello Mubarak <bellomuba.rak0@gmail.com>
2018-06-30 09:38:26 +0100

    ft-send-mail fully implemented

--- EmailSystem.py
+++ EmailSystem.py
@@ -1,34 +1,32 @@
 import smtplib
 from smtplib import SMTPAuthenticationError
-from email import encoders
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 print('Welcome to Email Sending System, kindly follow all instructions as go on')
 
 
-def send_mail(email, password, recipient, message):
+def mail_sender(sender, password, recipient, subject, message):
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-# server.starttls() not used because of the port, if .smtp with port 587 were used it'll be relevant
-    server.login(email, password)
-    server.sendmail(your_mail, recipient, message)
+    # server.starttls() not used because of the port, if .smtp with port 587 were used it'll be relevant
+    try:
+        server.login(sender, password)
+    except Exception:
+        print('END Error validating your details.\nTry again later')
+    try:
+        response_text = MIMEMultipart()
+        response_text['To'] = recipient
+        response_text['From'] = sender
+        response_text['Subject'] = subject
+        response_text.attach(MIMEText(message, 'plain'))
+        server.sendmail(sender, recipient, message)
+        print('END Mail sent successfully!')
+    except SMTPAuthenticationError:
+        print('END Response not accepted by Google.')
+    except Exception:
+        print('END Error sendding mail. Please try agin later.')
     server.quit()
 
 
-
-your_mail = input('email: ')
-your_mail_password = input('password: ')
-print("If you're inputing multiple emails use a single space to separate them.")
-recipient_mail = input('recipient_mail: ')
-
-if ' ' in recipient_mail:
-    try:
-        recipient_mail = recipient_mail.split(' ')
-    except EOFError:
-        print('Error Occurred')
-    finally:
-        pass
-else:
+def mail_receiver():
     pass
-message_subject = input('Subject: ')
-message_ = input('Message: ')
-
-
---
+++ app.py
@@ -0,0 +1,36 @@
+import africastalking as afs
+from flask import Flask, request
+
+from EmailSystem import mail_sender
+
+USERNAME = 'sandbox'
+API_KEY = '5e09c729236f9fd4bb0cd92e4ede25576f366734b442a0bd5297eda12385698d'
+afs.initialize(USERNAME, API_KEY)
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    response = request.post['text']
+    responses = response.strip('*')
+    if response == '':
+        print('CON DialMail\nRead and send quick mails!\n1. Read mails\n2. Send a quick mail')
+    elif response == '1':
+        print('CON ')
+    elif response == '2':
+        print('CON DialMail\nEnter your gmail login details\n\nE-mail:')
+    elif len(responses) == 2:
+        if responses[0] == 2: print('CON DialMail\n\nPassword:')
+    elif len(responses) == 3:
+        if responses[0] == 2: print("CON DialMail\n\nRecipient's mail address:")
+    elif len(responses) == 4:
+        if responses[0] == 2: print('CON DialMail\n\nSubject:')
+    elif len(responses) == 5:
+        if responses[0] == 2: print('CON DialMail\n\nMessage body:')
+    elif len(responses) == 6:
+        if responses[0] == 2: mail_sender(*responses[1:])
+    print('END An error occurred. Try again later')
+
+
+if __name__ == '__main__':
+    app.run()
